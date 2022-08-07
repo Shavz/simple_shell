5f4c575d3539227edb5973ac4181e32200272036
Shavz <104979137+Shavz@users.noreply.github.com>
2022-08-07 22:04:41 +0200

    Create more_tools.c

---
+++ more_tools.c
@@ -0,0 +1,70 @@
+#include "shell.h"
+
+/**
+ * _itoa - convert integer to array
+ * @n: the given number
+ *
+ * Return: a pointer to the null terminated string
+ */
+char *_itoa(unsigned int n)
+{
+	int len = 0, i = 0;
+	char *s;
+
+	len = intlen(n);
+	s = malloc(len + 1);
+	if (!s)
+		return (NULL);
+	*s = '\0';
+	while (n / 10)
+	{
+		s[i] = (n % 10) + '0';
+		n /= 10;
+		i++;
+	}
+	s[i] = (n % 10) + '0';
+	array_rev(s, len);
+	s[i + 1] = '\0';
+	return (s);
+}
+/**
+ * _atoi - converts character to integer
+ * @c: the given character
+ *
+ * Return: An integer
+ */
+int _atoi(char *c)
+{
+	unsigned int val = 0;
+	int sign = 1;
+
+	if (c == NULL)
+		return (0);
+	while (*c)
+	{
+		if (*c == '-')
+			sign *= (-1);
+		else
+			val = (val * 10) + (*c - '0');
+		c++;
+	}
+	return (sign * val);
+}
+
+/**
+ * intlen - Determine the number of digit int integer
+ * @num: the given number
+ *
+ * Return: the length of the integer
+ */
+int intlen(int num)
+{
+	int len = 0;
+
+	while (num != 0)
+	{
+		len++;
+		num /= 10;
+	}
+	return (len);
+}
