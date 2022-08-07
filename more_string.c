1d2c3b116b9a4f9748218d30f919ea0b58d60afd
Shavz <104979137+Shavz@users.noreply.github.com>
2022-08-07 22:03:43 +0200

    Update more_string.c

--- more_string.c
+++ more_string.c
@@ -17,3 +17,17 @@ char *_strcpy(char *dest, char *source)
 	dest[i] = '\0';
 	return (dest);
 }
+/**
+ * _isalpha - check if the input is a letter
+ * @c: the character to be checked
+ *
+ * Return: 1 if letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (SUCCESS);
+	}
+	return (FAIL);
+}
