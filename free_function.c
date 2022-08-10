8bfcb19edf4b6c7340b8f5f80fcb0c692d2c2142
Shavz <104979137+Shavz@users.noreply.github.com>
2022-08-10 10:34:26 +0200

    Update free_function.c

--- free_function.c
+++ free_function.c
@@ -8,14 +8,14 @@
 */
 void free_buffers(char **buf)
 {
-	int i = 0;
+int i = 0;
 
-	if (!buf || buf == NULL)
-		return;
-	while (buf[i])
-	{
-		free(buf[i]);
-		i++;
-	}
-	free(buf);
+if (!buf || buf == NULL)
+return;
+while (buf[i])
+{
+free(buf[i]);
+i++;
+}
+free(buf);
 }
